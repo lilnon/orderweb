8ff0683b0d36c3fb6fea9f417cffcb64651a1b69
dornathorn <drnatorntj@gmail.com>
2024-09-08 12:15:33 +0700

    database

--- shop.sql
+++ shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 03, 2024 at 08:22 AM
+-- Generation Time: Sep 08, 2024 at 05:14 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.3.11
 
@@ -32,21 +32,21 @@ CREATE TABLE `menu` (
   `name` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `image_url` varchar(255) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `status` enum('Not Finished','Finished') DEFAULT 'Not Finished'
+  `price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `menu`
 --
 
-INSERT INTO `menu` (`id`, `name`, `description`, `image_url`, `price`, `status`) VALUES
-(1, 'Sushi Roll', 'Delicious sushi roll with fresh ingredients.', 'https://example.com/sushi.jpg', 12.99, 'Not Finished'),
-(2, 'Ramen', 'Hot and spicy ramen with pork and vegetables.', 'https://example.com/ramen.jpg', 9.99, 'Not Finished'),
-(3, 'Tempura', 'Crispy tempura with a variety of vegetables and seafood.', 'https://example.com/tempura.jpg', 8.99, 'Not Finished'),
-(4, 'pasld[pasld', 'asdla[pdlp[', 'uploads/spidermonkey.jpg', 123123.00, 'Not Finished'),
-(5, 'pasld[pasld', 'asdla[pdlp[', 'uploads/spidermonkey.jpg', 123123.00, 'Not Finished'),
-(6, 'lilnon', 'rakbua', 'uploads/shutterstock_1576424071.png', 1000000.00, 'Not Finished');
+INSERT INTO `menu` (`id`, `name`, `description`, `image_url`, `price`) VALUES
+(7, 'ชุดผักรวม ', 'ในชุดจะมี ผักกาด ขึ้นฉ่าย กะหล่ำปลี ผักบุ้ง แครอท เห็ดชิเมจิขาว เห็ดหอม ข้าวโพด ', 'uploads/S__19767308.jpg', 89.00),
+(9, 'หมูสามชั้น สไลด์', 'จะมี หมูสามชั้นสไลด์ 10 ชิ้น', 'uploads/S__19767310.jpg', 69.00),
+(10, 'สันคอ สไลด์ ', 'สันคอหมู 5 ชิ้น', 'uploads/S__19767311.jpg', 69.00),
+(11, 'ลูกชิ้นปลา', 'ลูกชิ้นปลา 14 ชิ้น', 'uploads/S__19767314.jpg', 45.00),
+(12, 'หมูนุ่มหมักงา', 'หมูนุ่มหมักงา 10 ชิ้น', 'uploads/S__19767316.jpg', 59.00),
+(13, 'กุ้งขาว', 'กุ้งขาว 10 ตัว', 'uploads/S__19767318.jpg', 69.00),
+(14, 'กุ้งแม่น้ำ', 'กุ้งแม่น้ำ 3 ตัว', 'uploads/S__19767320.jpg', 350.00);
 
 -- --------------------------------------------------------
 
@@ -66,13 +66,10 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `menu_id`, `quantity`, `status`) VALUES
-(1, 1, 1, 'Finished'),
-(2, 2, 6, 'Not Finished'),
-(3, 3, 17, 'Not Finished'),
-(4, 6, 7, 'Finished'),
-(5, 5, 1, 'Not Finished'),
-(6, 5, 1, 'Finished'),
-(7, 5, 1, 'Not Finished');
+(40, 7, 1, 'Finished'),
+(41, 9, 1, 'Finished'),
+(42, 12, 1, 'Finished'),
+(43, 13, 1, 'Finished');
 
 -- --------------------------------------------------------
 
@@ -95,7 +92,9 @@ INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
 (16, 'bua', '123', 'admin'),
 (17, 'non', '123', 'user'),
 (18, 'member', '123', 'member'),
-(19, 'employee', '123', 'employee');
+(19, 'employee', '123', 'employee'),
+(20, 'user', '123', 'user'),
+(21, 'admin', '123', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -129,19 +128,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Constraints for dumped tables
